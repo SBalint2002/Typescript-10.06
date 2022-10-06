5ffefbe7f79d4a1452e0717e313f8cdd7d0f1929
Sárándi <sarandi.balint@gmail.com>
2022-10-06 12:23:39 +0200

    7-nel nagyobb ertekelesu konyvek

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Book_1 = require("./Book");
 let lista = [];
+console.log("\n30 db könyv:");
 for (let i = 0; i < 30; i++) {
     var rkonyv = Math.floor(Math.random() * 10 + 1);
     lista.push(new Book_1.Book("Book#" + i, rkonyv));
 }
 for (let i = 0; i < lista.length; i++) {
     console.log(lista[i]);
+}
+console.log("\n7-nél nagyobb értékelésű könyvek:");
+for (let i = 0; i < lista.length; i++) {
+    if (lista[i].rating > 7) {
+        console.log(lista[i]);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { Book } from "./Book";
 
 let lista: Array<Book> = [];
 
+console.log("\n30 db könyv:");
 for(let i = 0; i < 30; i++){
     var rkonyv = Math.floor(Math.random()*10+1);
     lista.push(new Book("Book#" + i, rkonyv));
@@ -13,4 +14,12 @@ for(let i = 0; i < 30; i++){
 
 for(let i = 0; i < lista.length; i++){
     console.log(lista[i]);
+}
+
+console.log("\n7-nél nagyobb értékelésű könyvek:");
+
+for(let i = 0; i < lista.length; i++){
+    if(lista[i].rating > 7){
+        console.log(lista[i]);
+    }
 }
